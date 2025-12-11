e218244b7e93c92f190f124c847c14b107033cda
issam liqali <liqaliissam@gmail.com>
2025-12-11 11:38:01 +0100

    test with toml file

--- toml/src/test/groovy/io/micronaut/toml/env/TomlPropertySourceLoaderSpec.groovy
+++ toml/src/test/groovy/io/micronaut/toml/env/TomlPropertySourceLoaderSpec.groovy
@@ -2,56 +2,21 @@ package io.micronaut.toml.env
 
 import io.micronaut.context.ApplicationContext
 import spock.lang.Specification
-import io.micronaut.context.env.PropertySource
 
 
 class TomlPropertySourceLoaderSpec extends Specification {
 
-    void "test toml property source loader"() {
-        given:
-        def loader = new TomlPropertySourceLoader()
-
-        def testToml = '''\
-[dataSource]
-jmxExport = true
-username = "sa"
-password = "test"
-'''
-
-        def appToml = '''\
-[hibernate]
-cache.queries = false
-
-[dataSource]
-pooled = true
-driverClassName = "org.h2.Driver"
-username = "sa"
-password = "test"
-something = [1, 2]
-'''
-
-        def map1 = loader.read("application-test", new ByteArrayInputStream(testToml.bytes))
-        def map2 = loader.read("application", new ByteArrayInputStream(appToml.bytes))
-
-        def ps1 = PropertySource.of("application-test", map1)
-        def ps2 = PropertySource.of("application", map2)
-
+    void "test TOML file is automatically loaded via environment"() {
+        when:
         ApplicationContext ctx = ApplicationContext.builder()
-                .environments("test")
+                .environments("mytest1")
                 .start()
 
-        ctx.environment.addPropertySource(ps1)
-        ctx.environment.addPropertySource(ps2)
-
-        when:
-        ctx.environment.refresh()
-
         then:
-        !ctx.environment.get("hibernate.cache.queries", Boolean).get()
-        ctx.environment.get("data-source.pooled", Boolean).get()
-        ctx.environment.get("data-source.password", String).get() == 'test'
-        ctx.environment.get("data-source.jmx-export", boolean).get()
-        ctx.environment.get("data-source.something", List).get() == [1,2]
+        ctx.environment.get("data-source.username", String).get() == "sa"
+        ctx.environment.get("data-source.password", String).get() == "secret"
+        ctx.environment.get("data-source.jmx-export", Boolean).get()
+        !ctx.environment.get("hibernate.cache-queries", Boolean).get()
 
         cleanup:
         ctx.close()
