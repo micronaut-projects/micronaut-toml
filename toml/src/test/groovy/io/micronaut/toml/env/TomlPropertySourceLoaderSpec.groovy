1413f9a5f7216069ad17ca80fb07acc28a318567
issam liqali <liqaliissam@gmail.com>
2025-12-11 11:52:40 +0100

    correct test

--- toml/src/test/groovy/io/micronaut/toml/env/TomlPropertySourceLoaderSpec.groovy
+++ toml/src/test/groovy/io/micronaut/toml/env/TomlPropertySourceLoaderSpec.groovy
@@ -9,14 +9,15 @@ class TomlPropertySourceLoaderSpec extends Specification {
     void "test TOML file is automatically loaded via environment"() {
         when:
         ApplicationContext ctx = ApplicationContext.builder()
-                .environments("mytest1")
+                .environments("test")
                 .start()
 
         then:
-        ctx.environment.get("data-source.username", String).get() == "sa"
-        ctx.environment.get("data-source.password", String).get() == "secret"
+        !ctx.environment.get("hibernate.cache.queries", Boolean).get()
+        ctx.environment.get("data-source.pooled", Boolean).get()
+        ctx.environment.get("data-source.password", String).get() == 'test'
         ctx.environment.get("data-source.jmx-export", Boolean).get()
-        !ctx.environment.get("hibernate.cache-queries", Boolean).get()
+        ctx.environment.get("data-source.something", List).get() == [1, 2]
 
         cleanup:
         ctx.close()
