7e9e024a0a30634a9808c324410219bd0fcf3449
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-12-12 12:58:41 +0100

    Build: Update to Micronaut 5 (#298)
    * refactor test : Removed subclassing of DefaultEnvironment (now final) and use ApplicationContext
    
    * use jackson 3 imports
    
    * use jspecify annotations
    
    * update to micronaut 5.0.0-M2
    
    * test with toml file
    
    * correct test

--- toml/src/main/java/io/micronaut/toml/Parser.java
+++ toml/src/main/java/io/micronaut/toml/Parser.java
@@ -16,7 +16,7 @@
 package io.micronaut.toml;
 
 import io.micronaut.core.annotation.Internal;
-import io.micronaut.core.annotation.Nullable;
+import org.jspecify.annotations.Nullable;
 import io.micronaut.json.tree.JsonNode;
 
 import java.io.IOException;
--- toml/src/test/groovy/io/micronaut/toml/env/TomlPropertySourceLoaderSpec.groovy
+++ toml/src/test/groovy/io/micronaut/toml/env/TomlPropertySourceLoaderSpec.groovy
@@ -1,73 +1,25 @@
 package io.micronaut.toml.env
 
-import io.micronaut.context.ApplicationContextConfiguration
-import io.micronaut.context.env.DefaultEnvironment
-import io.micronaut.context.env.Environment
-import io.micronaut.context.env.PropertySourceLoader
-import io.micronaut.core.io.service.ServiceDefinition
-import io.micronaut.core.io.service.SoftServiceLoader
-import io.micronaut.jackson.core.env.JsonPropertySourceLoader
+import io.micronaut.context.ApplicationContext
 import spock.lang.Specification
 
+
 class TomlPropertySourceLoaderSpec extends Specification {
-    void "test toml property source loader"() {
-        given:
-        def serviceDefinition = Mock(ServiceDefinition)
-        serviceDefinition.isPresent() >> true
-        serviceDefinition.load() >> new JsonPropertySourceLoader()
 
-        Environment env = new DefaultEnvironment(new ApplicationContextConfiguration() {
-            @Override
-            List<String> getEnvironments() {
-                return ["test"]
-            }
-        }) {
-            @Override
-            protected SoftServiceLoader<PropertySourceLoader> readPropertySourceLoaders() {
-                GroovyClassLoader gcl = new GroovyClassLoader()
-                gcl.addURL(JsonPropertySourceLoader.getResource("/META-INF/services/io.micronaut.context.env.PropertySourceLoader"))
-                return new SoftServiceLoader<PropertySourceLoader>(PropertySourceLoader, gcl)
-            }
-
-            @Override
-            Optional<InputStream> getResourceAsStream(String path) {
-                if(path.endsWith('-test.toml')) {
-                    return Optional.of(new ByteArrayInputStream('''\
-[dataSource]
-jmxExport = true
-username = "sa"
-password = "test"
-'''.bytes))
-                }
-                else if(path.endsWith("application.toml")) {
-                    return Optional.of(new ByteArrayInputStream('''\
-[hibernate]
-cache.queries = false
-
-[dataSource]
-pooled = true
-driverClassName = "org.h2.Driver"
-username = "sa"
-password = "test"
-something = [1, 2]
-'''.bytes))
-                }
-                return Optional.empty()
-            }
-        }
-
-
+    void "test TOML file is automatically loaded via environment"() {
         when:
-        env.start()
+        ApplicationContext ctx = ApplicationContext.builder()
+                .environments("test")
+                .start()
 
         then:
-        !env.get("hibernate.cache.queries", Boolean).get()
-        env.get("data-source.pooled", Boolean).get()
-        env.get("data-source.password", String).get() == 'test'
-        env.get("data-source.jmx-export", boolean).get()
-        env.get("data-source.something", List).get() == [1,2]
+        !ctx.environment.get("hibernate.cache.queries", Boolean).get()
+        ctx.environment.get("data-source.pooled", Boolean).get()
+        ctx.environment.get("data-source.password", String).get() == 'test'
+        ctx.environment.get("data-source.jmx-export", Boolean).get()
+        ctx.environment.get("data-source.something", List).get() == [1, 2]
 
-
-
+        cleanup:
+        ctx.close()
     }
 }
