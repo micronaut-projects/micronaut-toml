334514d9068c82af084f0aa6dd3bb655ffe59987
sdelamo <sdelamo@users.noreply.github.com>
2026-05-14 08:24:43 +0000

    Deploying to gh-pages - 08:24:43

--- latest/api/package-search-index.js
+++ latest/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.toml"},{"l":"io.micronaut.toml.env"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.toml"},{"l":"io.micronaut.toml.env"},{"l":"io.micronaut.toml.info"}];updateSearchResults();
